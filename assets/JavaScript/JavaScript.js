668ea5b60e36e7ff395a58b01a0a76b22806fd6c
Shiva Rahimitabar 🙋‍♀️👩‍💻 <rahimitabarshiva@gmail.com>
2025-04-05 12:44:59 +0330

    Developed by : Shiva Rahimitabar
    Date : 04/05/2025

---
+++ assets/JavaScript/JavaScript.js
@@ -0,0 +1,10 @@
+function Shiva(a) {
+     return a
+}
+Shiva()
+let _myDiv= document.getElementById('myDivision')
+_myDiv.style.display='flex'
+_myDiv.style.justifyContent='center'
+_myDiv.style.alignItems='center'
+_myDiv.style.alignItems='center'
+_myDiv.style.fontSize='70px'
